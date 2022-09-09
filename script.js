8f74fbfd3a8a0d61d052dfcff7117e2f000db3a4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-09 19:10:02 +0100

    Updated script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas1');
+const main = document.getElementById('main');
+const ctx = canvas.getContext('2d');
+
+
+
+ctx.fillStyle = 'white';
+ctx.fillRect(20, 10, 250, 150);
