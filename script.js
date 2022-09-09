a5cbe0fd11931b72459a6b74cbb82f0b1735c0a1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-09 19:49:06 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ const ctx = canvas.getContext('2d');
 canvas.width = main.offsetWidth;
 canvas.height = main.offsetHeight;
 
+const mouse = {
+    x: null,
+    y: null,
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(event);
+    drawCircle();
+})
 
 
-ctx.fillStyle = 'red';
-ctx.strokeStyle = 'blue';
-ctx.beginPath();
-ctx.arc(200, 310, 100, 0, Math.PI * 2);
-ctx.stroke();
+function drawCircle() {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(360, 310, 50, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+}
+
+
 
