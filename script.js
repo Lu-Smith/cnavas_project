00c39b24805660d0ae28b568c631a86b26203524
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-09 19:33:18 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const canvas = document.getElementById('canvas1');
 const main = document.getElementById('main');
 const ctx = canvas.getContext('2d');
 
+canvas.width = main.offsetWidth;
+canvas.height = main.offsetHeight;
 
 
-ctx.fillStyle = 'white';
-ctx.fillRect(20, 10, 250, 150);
+
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'blue';
+ctx.beginPath();
+ctx.arc(200, 310, 100, 0, Math.PI * 2);
+ctx.stroke();
+
