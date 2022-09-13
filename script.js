11142161e835466c61794b69387422c12ac7be1b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-13 12:25:46 +0100

    Updated script.js
    Animated title

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+//title
+const text = document.getElementById('title');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "    " + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const spans = text.querySelectorAll('span')[char];
+    spans.classList.add('fade');
+    char++
+    if(char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+
+}
+
+
+
+//canvas
+
+
+
+
 const canvas = document.getElementById('canvas1');
 const main = document.getElementById('main');
 const ctx = canvas.getContext('2d');
