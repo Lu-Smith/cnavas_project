23f918bce040fb64385d96a469ec11791109e25c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-13 10:39:47 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -30,20 +30,22 @@ class Particle {
         //this.y = mouse.y;
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+
+        if (this.size > 0.1) this.size -= 0.05;
     }
     draw(){
         ctx.fillStyle = 'red';
         ctx.strokeStyle = 'yellow';
         ctx.lineWidth = 10;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 20, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.stroke();
         ctx.fill();
     }
@@ -56,12 +58,15 @@ function init(){
 }
 
 init();
-console.log(particlesArray);
 
 function handleParticle() {
     for (let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        if (particlesArray[i].size <= 0.1){
+            particlesArray.splice(i, 1);
+            i--;
+        }
     }
 }
 
@@ -69,6 +74,7 @@ function handleParticle() {
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleParticle();
+    console.log(particlesArray.length);
     requestAnimationFrame(animate);
 }
 
